4e6a94c6d1f578818ec822408995a66c9d4378c3
zhu <gr.zhu@qq.com>
2025-07-16 22:18:38 +0800

    chore: 更新 .gitignore 并添加新子目录到 CMakeLists.txt
    - 在 .gitignore 中添加 .cache 目录以忽略缓存文件
    - 在 CMakeLists.txt 中添加 qtoutlogger 子目录

---
+++ qtoutlogger/src/main.cpp
@@ -0,0 +1,39 @@
+﻿#include "mainwindow.h"
+#include "qdir.h"
+#include "qfile.h"
+#include "qmutex.h"
+#include <QDateTime>
+#include <QFileInfo>
+#include <QSettings>
+#include <QTextStream>
+#include <QtWidgets/QApplication>
+#include <qDebug>
+#include <qthread.h>
+
+#include "qylogger.h"
+#include <iostream>
+#include <thread>
+
+int main(int argc, char* argv[])
+{
+    // Set up logger with log file path, max file size (10MB), and max log files (3)
+    setup_logger("./log", 10 * 1024 * 1024, 3);
+
+    // std::thread t(
+    //     []()
+    //     {
+    //         for (int i = 0; i < 1e5; i++)
+    //         {
+    //             qDebug() << "sub thread:" << i;
+    //         }
+    //     });
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1e5; i++)
+    {
+        qDebug() << "main thread: " << i;
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms"
+              << std::endl;
+    // t.join();
+}
