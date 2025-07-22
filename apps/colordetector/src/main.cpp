729f3f2c9b3e6bd98d6af8f0f809d5b062f6d8cf
aiyolo <1044965211@qq.com>
2025-07-22 13:50:54 +0800

    feat(colordetector): 添加相位计算和 atan 性能测试功能
    - 新增 computeAngle 函数用于计算图像的相位
    - 添加 test_phase 函数进行相位计算测试
    - 实现 fastAtan 和 normalAtan 函数以比较 atan 性能
    - 在主函数中调用新增的测试函数并输出结果

--- apps/colordetector/src/main.cpp
+++ apps/colordetector/src/main.cpp
@@ -1,11 +1,17 @@
+#include <algorithm>
 #include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/core/cvdef.h>
 #include <opencv2/core/operations.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/opencv.hpp>
 #include <fmt/format.h>
+#include <qelapsedtimer.h>
 #include <sstream>
+#include <opencv2/core/hal/hal.hpp>
+#include <qtimer>
 
 // fmt print cv::Vec3i
 
@@ -28,19 +34,110 @@ void write_array_to_file(const std::string& filename, const cv::Mat& array)
     }
 }
 
+cv::Mat computeAngle(const cv::Mat& image)
+{
+    CV_Assert(image.channels() == 2);
+    cv::Mat angle(image.size(), CV_64F);
+
+    for (int i = 0; i < image.rows; i++) {
+        const auto xy_ptr = image.ptr<cv::Vec2d>(i);
+        auto angle_ptr = angle.ptr<double>(i);
+        for (int j = 0; j < image.cols; j++) {
+            const auto xy = xy_ptr[j];
+            auto ang = std::atan2(xy[0], xy[1]);
+            if (ang < 0) {
+                ang += CV_PI;
+            }
+            ang *= 180.0 / CV_PI;
+            angle_ptr[j] = ang;
+        }
+    }
+    return angle;
+}
+
+void test_phase()
+{
+    // create a two channel mat;
+    cv::Mat image(5, 5, CV_64FC2);
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            int val = i * image.cols + j;
+            image.at<cv::Vec2d>(i, j) = cv::Vec2d(val, 2 * val);
+        }
+    }
+    cv::print(image);
+    std::cout << std::endl;
+    auto angle = computeAngle(image);
+    cv::print(angle);
+    std::cout << std::endl;
+    std::vector<cv::Mat> channels;
+    cv::split(image, channels);
+    cv::Mat angle1;
+    cv::phase(channels[0], channels[1], angle1);
+    // cv::multiply(angle1, 180. / CV_PI, angle1);
+    cv::print(angle1);
+    std::cout << std::endl;
+    std::cout << image.dims << "," << image.size();
+
+    cv::Mat mat1(cv::Size(4, 1), CV_32FC1), mat2(cv::Size(4, 1), CV_32FC1),
+      mat3;
+    mat1.at<float>(0, 0) = 1;
+    mat2.at<float>(0, 0) = 1;
+    mat1.at<float>(0, 1) = 1;
+    mat2.at<float>(0, 1) = -1;
+    mat1.at<float>(0, 2) = -1;
+    mat2.at<float>(0, 2) = -1;
+    mat1.at<float>(0, 3) = -1;
+    mat2.at<float>(0, 3) = 1;
+    cv::phase(mat2, mat1, mat3);
+    std::cout << std::endl;
+    cv::print(mat3);
+}
+
+void fastAtan()
+{
+    double y1 = -2.33, x1 = 1.1, r;
+    cv::hal::fastAtan64f(&y1, &x1, &r, 1, false);
+}
+
+void normalAtan()
+{
+    double y1 = -2.33, x1 = 1.1, r;
+    r = std::atan2(y1, x1);
+}
+
 int main()
 {
-    fmt::print("Hello World!\n");
-    cv::Vec3i v1(1, 2, 3);
-    cv::Vec3i v2(4, 5, 6);
+    test_phase();
+    std::cout << std::endl;
+    std::cout << std::atan2(1, 1) << "," << std::atan2(1, -1) << ","
+              << std::atan2(-1, -1) << "," << std::atan2(-1, 1);
 
-    auto diff = v1 - v2;
+    std::cout << std::endl;
+    QElapsedTimer timer;
+    timer.start();
+    for (int i = 0; i < 100000000; ++i) {
+        fastAtan();
+    }
+    std::cout << "fastAtan: " << timer.elapsed() << std::endl;
+    QElapsedTimer timer1;
+    timer1.start();
+    for (int i = 0; i < 100000000; ++i) {
+        normalAtan();
+    }
+    std::cout << "normalAtan: " << timer1.elapsed() << std::endl;
 
-    cv::Vec3b b1(1, 2, 3);
-    cv::Vec3b b2(4, 5, 6);
-    // sub will cause saturate cast
-    auto diff0 = b2 - b1;
-    auto diff2 = cv::norm(b1 - b2);
-    auto diff3 = cv::normL2Sqr<uchar, uchar>(diff0.val, 3);
-    fmt::print("diff: {}\n", diff3);
+    //     fmt::print("Hello World!\n");
+    //     cv::Vec3i v1(1, 2, 3);
+    //     cv::Vec3i v2(4, 5, 6);
+
+    //     auto diff = v1 - v2;
+
+    //     cv::Vec3b b1(1, 2, 3);
+    //     cv::Vec3b b2(4, 5, 6);
+    //     // sub will cause saturate cast
+    //     auto diff0 = b2 - b1;
+    //     auto diff2 = cv::norm(b1 - b2);
+    //     auto diff3 = cv::normL2Sqr<uchar, uchar>(diff0.val, 3);
+    //     fmt::print("diff: {}\n", diff3);
 }
